d38be3085641d1583b34760ac6b9162aa092d0a3
Nakul Bageja <39912882+ketonium10@users.noreply.github.com>
2020-09-09 21:20:08 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
-      })
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url} target="_blank">
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
     }
 
     return (
       <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
-           <ul>
-            <li>Made with love by Nakul Bageja ♥️</li>
-           </ul>
-
+            <ul>
+              <li className="credits">Made with love by Nakul Bageja </li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
