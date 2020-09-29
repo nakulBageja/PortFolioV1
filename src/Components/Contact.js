c8f4f094521a16decea6f28d6178340c74936dd3
unknown <nbageja@gmail.com>
2020-09-29 20:26:38 +0530

    Added functionality of emailJs to contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,97 +1,160 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import * as emailjs from "emailjs-com";
 const Contact = ({ data }) => {
-   const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
-   const [name, setName] = useState('');
-   const [subject, setSubject] = useState('');
-   const [email, setEmail] = useState('');
-   const [message, setMessage] = useState('');
+  const [url, setUrl] = useState(
+    "mailto:test@example.com?subject=subject&body=body"
+  );
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-   console.log(data)
-    const handleClick = (e) => {
-       e.preventDefault();
-      window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
-    }
-    
+  const resetForm = () => {
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  };
+  const handleClick = e => {
+    e.preventDefault();
+    console.log(process.env.REACT_APP_USER_ID);
+    let templateParams = {
+      from_email: email,
+      to_email: "nakulbageja@gmail.com",
+      subject: subject,
+      message_html: message,
+      from_name: name,
+      to_name: "Nakul"
+    };
+    //Service of emailJs added
+    emailjs
+      .send(
+        "gmail",
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        function(response) {
+          console.log("Success", response.status, response.text);
+          alert("Thank you for your message, I will respond soon");
+          resetForm();
+        },
+        function(error) {
+          console.log("Failed..", error);
+          resetForm();
+        }
+      );
+  };
 
-    return (
-      <section id="contact">
+  return (
+    <section id="contact">
+      <div className="row section-head">
+        <div className="two columns header-col">
+          <h1>
+            <span>Get In Touch.</span>
+          </h1>
+        </div>
 
-         <div className="row section-head">
-
-            <div className="two columns header-col">
-
-               <h1><span>Get In Touch.</span></h1>
-
-            </div>
-
-            <div className="ten columns">
-
-                  <p className="lead">{data?.message}</p>
-
-            </div>
-
-         </div>
-
-         <div className="row">
-            <div className="eight columns">
-
-               <form id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input value={name} type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value={email} type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input value={subject} type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button type='submit' onClick={handleClick} className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>My Contact</h4>
-					   <p className="address">
-						   {data?.email}<br />
-						   <span>{data?.phone}</span>
-					   </p>
-				   </div>
-
-               <div className="widget widget_tweets">
-
-		         </div>
-            </aside>
+        <div className="ten columns">
+          <p className="lead">{data?.message}</p>
+        </div>
       </div>
-   </section>
-    );
-}
+
+      <div className="row">
+        <div className="eight columns">
+          <form id="contactForm" name="contactForm">
+            <fieldset>
+              <div>
+                <label htmlFor="contactName">
+                  Your Name <span className="required">*</span>
+                </label>
+                <input
+                  value={name}
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactName"
+                  name="contactName"
+                  onChange={e => setName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactEmail">
+                  Your Email <span className="required">*</span>
+                </label>
+                <input
+                  value={email}
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactEmail"
+                  name="contactEmail"
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactSubject">Subject</label>
+                <input
+                  value={subject}
+                  type="text"
+                  defaultValue=""
+                  size="35"
+                  id="contactSubject"
+                  name="contactSubject"
+                  onChange={e => setSubject(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactMessage">
+                  Message <span className="required">*</span>
+                </label>
+                <textarea
+                  value={message}
+                  onChange={e => setMessage(e.target.value)}
+                  cols="50"
+                  rows="15"
+                  id="contactMessage"
+                  name="contactMessage"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit" onClick={handleClick} className="submit">
+                  Submit
+                </button>
+                <span id="image-loader">
+                  <img alt="" src="images/loader.gif" />
+                </span>
+              </div>
+            </fieldset>
+          </form>
+
+          <div id="message-warning"> Error boy</div>
+          <div id="message-success">
+            <i className="fa fa-check"></i>Your message was sent, thank you!
+            <br />
+          </div>
+        </div>
+
+        <aside className="four columns footer-widgets">
+          <div className="widget widget_contact">
+            <h4>My Contact</h4>
+            <p className="address">
+              {data?.email}
+              <br />
+              <span>{data?.phone}</span>
+            </p>
+          </div>
+
+          <div className="widget widget_tweets"></div>
+        </aside>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,56 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-   render() {
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var occupation = this.props.data.occupation;
+      var city = this.props.data.address.city;
+      var country = this.props.data.address.country;
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url} target="_blank">
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
+    }
 
-      if (this.props.data) {
-         var name = this.props.data.name;
-         var occupation = this.props.data.occupation;
-         var city = this.props.data.address.city;
-         var country = this.props.data.address.country;
-         var networks = this.props.data.social.map(function (network) {
-            return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
-         })
-      }
+    return (
+      <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
 
-      return (
-         <header id="home">
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#testimonials">
+                Recommendations
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-            <nav id="nav-wrap">
+        <div className="row banner">
+          <div className="banner-text">
+            <h1>NAMASTE!</h1>
+            <h2 className="responsive-headline">I'm {name}</h2>
+            <h3>
+              A <span>{occupation}</span> based in{" "}
+              <span>
+                {city}, {country}
+              </span>
+              .{" "}
+            </h3>
+            <hr />
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
 
-               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-               <ul id="nav" className="nav">
-                  <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-                  <li><a className="smoothscroll" href="#about">About</a></li>
-                  <li><a className="smoothscroll" href="#resume">Resume</a></li>
-                  <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                  <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-                  <li><a className="smoothscroll" href="#contact">Contact</a></li>
-               </ul>
-
-            </nav>
-
-            <div className="row banner">
-               <div className="banner-text">
-                  <h1>NAMASTE!</h1>
-                  <h2 className="responsive-headline">I'm {name}</h2>
-                  <h3>A <span>{occupation}</span> based in <span>{city}, {country}</span>. </h3>
-                  <hr />
-                  <ul className="social">
-                     {networks}
-                  </ul>
-               </div>
-            </div>
-
-            <p className="scrolldown">
-               <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-            </p>
-
-         </header>
-      );
-   }
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
 }
 
 export default Header;
